b977591193919f0a4766a9a965a9b1292ecce6fa
sylvia <1255915270@qq.com>
2017-12-27 22:19:45 +0800

    add-react-router-dom

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import BasicExample from './src/Basic';
 
 class APP extends Component{
     render(){
         return(
-            <div>Hello React!</div>
+            <BasicExample/>
         )
     }
 }
